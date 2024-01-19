4406c6ae5ec1ce1e510a3b128711f62e28be7520
snower <sujian199@gmail.com>
2024-01-19 15:19:38 +0800

    change version to 0.0.8

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 import os
 from setuptools import find_packages, setup
 
-version = "0.0.7"
+version = "0.0.8"
 
 if os.path.exists("README.md"):
     if sys.version_info[0] >= 3:
@@ -38,7 +38,7 @@ setup(
     install_requires=[
         "pyyaml>=5.1.2",
         "sqlglot>=11.5.5,<12",
-        "syncanysql>=0.1.12",
+        "syncanysql>=0.1.14",
         "mysql-mimic>=2.2.3"
     ],
     extras_require={
--- syncanyserver/__init__.py
+++ syncanyserver/__init__.py
@@ -2,5 +2,5 @@
 # 2023/5/4
 # create by: snower
 
-version = "0.0.7"
-version_info = (0, 0, 7)
+version = "0.0.8"
+version_info = (0, 0, 8)
